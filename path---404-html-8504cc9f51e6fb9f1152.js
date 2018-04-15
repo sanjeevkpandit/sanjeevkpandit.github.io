99c730f2014f951eb605b9df3bc102ba112fb8bc
Sanjeev Kumar Pandit <binarysanjeev@gmail.com>
2018-04-15 19:38:14 +0545

    Add public folder

---
+++ path---404-html-8504cc9f51e6fb9f1152.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xa2868bfb69fc],{385:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Sanjeev Kumar Pandit",subtitle:"#FullStackDeveloper #FootballFan",copyright:"© All rights reserved.",menu:[{label:"Articles",path:"/"},{label:"About me",path:"/about/"},{label:"Contact me",path:"/contact/"}],author:{name:"John Doe",email:"#",telegram:"#",twitter:"#",github:"#",rss:"#",vk:"#"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-html-8504cc9f51e6fb9f1152.js.map
