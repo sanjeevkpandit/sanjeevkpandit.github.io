16793581efedacb67d611af2925dc3d7de95b06b
Sanjeev Kumar Pandit <krsanjeevpandit@gmail.com>
2026-05-03 19:47:48 +0545

    Upgrade Hugo to 0.160.1

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,13 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+required_hugo_version="0.160.1"
+installed_hugo_version="$(hugo version | sed -n 's/^hugo v\([0-9.]*\).*/\1/p')"
+if [ "$installed_hugo_version" != "$required_hugo_version" ]; then
+	printf "Expected Hugo %s, found %s\n" "$required_hugo_version" "${installed_hugo_version:-unknown}"
+	exit 1
+fi
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
